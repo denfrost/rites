335a0e983b6dcb3940fc6d8f2770ca30b2964c6e
Martin Holtkamp <mholt012@gmail.com>
2018-05-13 13:44:22 -0400

    Adding some initial gameplay classes. Setup editor default map and game default map.

---
+++ Source/Rites/FighterController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FighterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RITES_API AFighterController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
